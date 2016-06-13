35c8bcf9791e412c74e393833eb315267027e78c
Louis Tur <louis.tur@gmail.com>
2016-06-13 18:09:35 -0400

    not entirely happy with the intro fade, but it gets the job done for now

--- SRAnimationTests/AppDelegate.swift
+++ SRAnimationTests/AppDelegate.swift
@@ -28,75 +28,55 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
 
-    let tempView: UIView = UIView(frame: UIScreen.mainScreen().bounds)
+    let tempView: UIView = UIView(frame: self.window!.frame)
     let monkeyFace: UIImageView = UIImageView(image: UIImage(named: "monkey_face"))
     tempView.backgroundColor = Colors.BaseOrange
     tempView.addSubview(monkeyFace)
-    tempView.snp_makeConstraints { (make) in
+    monkeyFace.snp_makeConstraints { (make) in
       make.center.equalTo(tempView)
     }
+    self.window!.addSubview(tempView)
+    self.window!.hidden = false
     
     introView = tempView
-    self.window!.addSubview(tempView)
+    let root: UIViewController = ViewController()
+    root.view.addSubview(tempView)
+    
     animateView(monkeyFace)
     
-        UIApplication.sharedApplication().statusBarHidden = true
+    UIApplication.sharedApplication().statusBarHidden = true
+    
+    self.window!.rootViewController = root
+    self.window!.makeKeyAndVisible()
     return true
   }
   
   func animateView(view: UIView) {
     
-    UIView.animateKeyframesWithDuration(0.6, delay: 0.1, options: [], animations: { 
+    UIView.animateKeyframesWithDuration(1.2, delay: 0.0, options: [.CalculationModeLinear], animations: {
       
-      UIView.addKeyframeWithRelativeStartTime(0.0, relativeDuration: 0.33, animations: { 
-        view.transform = CGAffineTransformMakeScale(0.8, 0.8)
-      })
-      
-      UIView.addKeyframeWithRelativeStartTime(0.33, relativeDuration: 0.33, animations: {
+      UIView.addKeyframeWithRelativeStartTime(0.00, relativeDuration: 0.5, animations: {
         view.transform = CGAffineTransformMakeScale(1.0, 1.0)
       })
       
-      UIView.addKeyframeWithRelativeStartTime(0.67, relativeDuration: 0.33, animations: {
+      UIView.addKeyframeWithRelativeStartTime(0.50, relativeDuration: 0.25, animations: {
+        view.transform = CGAffineTransformMakeScale(0.8, 0.8)
+      })
+      
+      UIView.addKeyframeWithRelativeStartTime(0.75, relativeDuration: 0.25, animations: {
         view.transform = CGAffineTransformMakeScale(4.0, 4.0)
-        view.alpha = 0.0
+        self.introView!.alpha = 0.0
       })
       
     }) { (complete: Bool) in
       if complete == true {
         self.introView!.removeFromSuperview()
-        self.window!.rootViewController = ViewController()
-        self.window!.makeKeyAndVisible()
-
       }
     }
     
     
   }
-  
-  func animateMask(mask: CALayer) {
-    
-    let keyFrameAnimation = CAKeyframeAnimation(keyPath: "bounds")
-    keyFrameAnimation.delegate = self
-    keyFrameAnimation.duration = 0.6
-    keyFrameAnimation.beginTime = CACurrentMediaTime() + 0.5
-    keyFrameAnimation.timingFunctions = [CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut), CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)]
-    let initalBounds = NSValue(CGRect: mask.bounds)
-    let secondBounds = NSValue(CGRect: CGRect(x: 0, y: 0, width: 90, height: 73))
-    let finalBounds = NSValue(CGRect: CGRect(x: 0, y: 0, width: 1600, height: 1300))
-    keyFrameAnimation.values = [initalBounds, secondBounds, finalBounds]
-    keyFrameAnimation.keyTimes = [0, 0.3, 1]
-    mask.addAnimation(keyFrameAnimation, forKey: "bounds")
-    
-  }
 
-  override func animationDidStop(anim: CAAnimation, finished flag: Bool) {
-//      self.window?.layer.mask = nil
-    if flag == true {
-      UIView.animateWithDuration(0.15, animations: { 
-        self.introView?.alpha = 0.0
-      })
-    }
-  }
   
   func applicationWillResignActive(application: UIApplication) {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
